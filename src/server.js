83161652c4a4da3a9a8282f3a1f1a9ddef6cb014
Chazu <chaz@plaidpotion.com>
2014-05-23 18:47:46 -0400

    remove cruft

--- src/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-
-var app = express();
-console.log(__dirname);
-app.use(express.static(__dirname));
-
-console.log("/","Glyph browser listening on port 8080");
-app.listen(8080);
