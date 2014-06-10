e5d80efb6b22740c1230f98054a40f3f3ca4bb28
Chazu <chaz@plaidpotion.com>
2014-06-09 22:23:00 -0400

    adding tests

--- test/moduleTest.js
+++ test/moduleTest.js
@@ -1,4 +1,20 @@
-var hershey = require('./../dist/hershey.js');
+var Hershey = require('./../dist/hershey.js');
 
 describe('hershey module', function() {
+
+  it('should have a glyphs function', function() {
+    expect(typeof Hershey.glyph).toBe('function');
+  });
+
+  it('should have a glyphById function', function() {
+    expect(typeof Hershey.glyphById).toBe('function');
+  });
+
+  it('should have function stringVertices', function() {
+    expect(typeof Hershey.stringVertices).toBe('function');
+  });
+
+  describe("function stringVertices", function() {
+    expect(Hershey.stringVertices("A")["vertices"]).toBeDefined();
+  });
 });
